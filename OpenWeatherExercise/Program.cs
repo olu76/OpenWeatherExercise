edc462b9f500c8247049be9581f69a24bd4731fb
olumide azeez afolabi <midas44000@yahoo.com>
2021-12-03 04:22:15 -0600

    initial commit

---
+++ OpenWeatherExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace OpenWeatherExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode:");
+            var zipcode = Console.ReadLine();
+            Console.WriteLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipcode}&units=imperial$appid={APIkey}";
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+        }
+    }
+}
